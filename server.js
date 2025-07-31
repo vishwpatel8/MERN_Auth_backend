8786d7c3f614a71c360054336fae1d9c12399ace
vishw patel <vishwprofessional@gmail.com>
2025-07-31 12:17:44 +0530

    Added dynamic route handling

--- server.js
+++ server.js
@@ -1,5 +1,49 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connect from "./src/db/connect.js";
+import cookieParser from "cookie-parser";
+import fs from "node:fs";
+
+dotenv.config();
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// routes 
+const routeFiles= fs.readdirSync("./src/routes");
+routeFiles.forEach((file)=> {
+  import(`./src/routes/${file}`).then((route)=> {
+    app.use("/api/v1", route.default);
+  }).catch((err)=> {
+    console.log("Failed to Log Route File", err);
+  });
+});
+
+console.log(routeFiles);
+
 const server = async () => {
-  console.log("Server is Running...");
+  try {
+    await connect();
+    app.listen(port, () => {
+      console.log(`server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log("Failed to start server...", error.Message);
+    process.exit(1);
+  }
 };
 
 server();
